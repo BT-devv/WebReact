75c7636387651eeb5cf6bdd9d95574832e8b5f6b
Phamvan22 <johndoe@example.com>
2023-06-22 13:53:55 +0700

    v

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import{Link} from 'react-router-dom';
+import axios from "axios";
 
 const Register = () =>{
+    //DỮ LIỆU ĐẦU VÀO
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
+
+    // XỬ LÝ SỰ THAY ĐỔI
+    const handleChange = e =>{
+        setInputs(prev =>({...prev, [e.target.name]: e.target.value}))
+
+    }
+
+    //DỮ LIỆU CUỐI
+    const handleSbmit = async e =>{
+        //KẾT THÚC, NGĂN CHẶN MẶC ĐỊNH
+        e.prevenDefault()
+        try {
+            const res  = await axios.post("/authController/register", inputs)
+            console.log(res);
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
     return(
         <div className='auth'>
             <h1>Register</h1>
             <form>
-                <input required type="text" placeholder='Username'/>
-                <input required type="email" placeholder='Email'/>
-                <input required type="password" placeholder='Password'/>
-                <input required type="password" placeholder='Re-Password'/>
-                <button>
+                <input required type="text" placeholder='Username' name='username' onChange={handleChange}/>
+                <input required type="email" placeholder='Email' name='email' onChange={handleChange}/>
+                <input required type="password" placeholder='Password' name='password' onChange={handleChange}/>
+                <input required type="password" placeholder='Re-Password' name='re-password' onChange={handleChange}/>
+                <button onClick={handleSbmit}>
                     Register
                 </button>
                 <p>This is erro!!!</p>
