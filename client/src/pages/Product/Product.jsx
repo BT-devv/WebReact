630e43df48c85ac978643ba06181376d6151d2dc
Phamvan22 <johndoe@example.com>
2023-08-09 08:58:27 +0700

    layout product

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -3,13 +3,25 @@ import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import { Button, Grid, TextField, MenuItem } from "@mui/material";
 const Product = () => {
   const [selectImg, setSelectImg] = useState(0);
   const [selecQuantity, setQuantity] = useState(0);
+  const [selectedSize, setSelectedSize] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
   const img = [
     "	https://image.uniqlo.com/UQ/ST3/vn/imagesgoods/455492/item/vngoods_69_455492.jpg?",
     "	https://image.uniqlo.com/UQ/ST3/vn/imagesgoods/455492/sub/vngoods_455492_sub7.jpg?",
   ];
+  const sizes = ["XS", "S", "M", "L", "XL"];
+  const colors = [
+    { name: "Brown", code: "#440000" },
+    { name: "Navy", code: "#006400" },
+    { name: "Gray", code: "#828282" },
+    { name: "Black", code: "#333333" },
+    { name: "White", code: "#FFFFFF" },
+  ];
+
   return (
     <div className="productDetails">
       <div className="left">
@@ -30,6 +42,38 @@ const Product = () => {
           amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt ut
           labore etdolore.
         </p>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={6}>
+            <TextField
+              select
+              label="Size"
+              value={selectedSize}
+              onChange={(e) => setSelectedSize(e.target.value)}
+              fullWidth
+            >
+              {sizes.map((size) => (
+                <MenuItem key={size} value={size}>
+                  {size}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={6}>
+            <div className="color-buttons">
+              {colors.map((color) => (
+                <Button
+                  key={color.name}
+                  className={`color-button ${selectedColor === color.name ? "active" : ""}`}
+                  style={{ backgroundColor: color.code }}
+                  onClick={() => setSelectedColor(color.name)}
+                ></Button>
+              ))}
+            </div>
+            <div className="color-name">
+              {selectedColor && `Selected: ${selectedColor}`}
+            </div>
+          </Grid>
+        </Grid>
         <div className="quantity">
           <button
             onClick={() => setQuantity((prev) => (prev === 0 ? 0 : prev - 1))}
@@ -64,6 +108,7 @@ const Product = () => {
           <hr />
           <span>FAQ</span>
         </div>
+
       </div>
     </div>
   );
